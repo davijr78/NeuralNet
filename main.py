ad18672d28a65f91e0869c16557037ea17996846
davi <davi.starepravo.jr@outlook.com>
2026-02-13 22:14:51 -0300

    yeet

--- main.py
+++ main.py
@@ -3,25 +3,43 @@ import numpy as np
 def exact_value(x):
     return x**2 - 2*x + 3
 
-X_Training = np.random.randint(low=-1000, high=1000, size=(100, 1))
-X_Testing = np.random.randint(low=-1000, high=1000, size=(100, 1))
-W = np.random.rand(10,1)
-Y = np.random.rand(1, )
-B = np.random.rand(10,1)
+X_Training = np.random.rand(250, 1)
+X_Testing = np.random.rand(250, 1)
+
+W1 = np.random.rand(16, 1)
+W2 = np.random.rand(1, 16)
+B1 = np.random.rand(16, 1)
+B2 = np.random.rand(1, 1)
+C = 0
 
 def activation(x):
     return 1 / (1 + np.exp(-x))
 
 def forward_pass(x, w, b):
-    return activation(np.dot(x, w) + b
-
-
+    return np.dot(x, w) + b
 
 def cost_function(x):
     pass
 
+def back_prop(x, w, b):
+    pass
 
-print(forward_pass(X[1], W, B))
+def train(x, w1, b1, w2, b2, epochs):
+    for epoch in range(epochs):
+        for i in x:
+            z1 = forward_pass(i, w1, b1)
+            h1 = activation(z1)
+            o = forward_pass(h1, w2, b2)
+            return o
+
+def test(x, w1, w2, b1, b2):
+    pass
+
+def main():
+    pass
+
+
+
 
 
 
